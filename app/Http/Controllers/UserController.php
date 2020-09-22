ae2571994f4b071489d0df3add7a66f8d1214579
Rubyet <rittick.2012@gmail.com>
2020-09-23 03:36:28 +0600

    Minor changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,40 +11,35 @@ class UserController extends Controller
     public function register(Request $request)
     {
         try {
-            
-
-            
         } catch (\Throwable $th) {
             //return response(['error'=>'No valid data']);
-        } 
-        $validationData= $request-> validate
-        (
+        }
+        $validationData = $request->validate(
             [
-                'name'=>'required|max:55',
-                'email'=>'email|require|unique:users',
-                'password'=>'required|confirmed',
+                'name' => 'required|max:55',
+                'email' => 'email|require|unique:users',
+                'password' => 'required|confirmed',
             ]
         );
         $user = User::create($validationData);
 
         $accessToken = $user->createToken('authToken')->accessToken;
 
-        return response(['user'=>$user,'accessToken'=>$accessToken]);
-}
-
-    public function login()
-    { 
-        
+        return response(['user' => $user, 'accessToken' => $accessToken]);
     }
 
-    public function details() 
-    { 
-        $user = Auth::user(); 
-        return response()->json(['success' => $user], $this-> successStatus); 
-    } 
+    public function login()
+    {
+    }
+
+    public function details()
+    {
+        $user = Auth::user();
+        return response()->json(['success' => $user], $this->successStatus);
+    }
 
     public function test()
     {
-        return (['Test'=>'12345','hola'=>'BD']);
+        return (['Test' => '12345', 'hola' => 'BD']);
     }
 }
